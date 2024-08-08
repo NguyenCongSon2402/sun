d0881a1b2b6b567fb14ccf3f9a1d4684de316b14
nguyễn công sơn <91744029+NguyenCongSon2402@users.noreply.github.com>
2024-08-08 23:15:24 +0700

    Update myJs.js

--- js/myJs.js
+++ js/myJs.js
@@ -138,7 +138,7 @@ $(document).ready(function () {
           text: textConfig.text11,
           confirmButtonColor: "#83d0c9",
           onClose: () => {
-            window.location = "https://www.messenger.com/e2ee/t/6426489680786273";
+            window.location = "https://m.me/sonnguyen762";
           },
         });
       }
